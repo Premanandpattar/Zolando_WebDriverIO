d1bef8bc539eeca6b56dd88d9b87b1d867dc3fd4
Premanandpattar <premanandpattar@gmail.com>
2022-10-19 13:15:33 +0530

    new

---
+++ test/specs/UserLogInLog.js
@@ -0,0 +1,37 @@
+// // const userLogInPage = require(`../pageobjects/userLogInPage`)
+// // const userCommanPage = require("../pageobjects/userCommanPage")
+
+// // describe(`UserLogInLog`,()=>{
+
+// //     it("log in user", async () => {
+// //         var UN = "anuj.lpu1@gmail.com"
+// //         var pass = "Test@123"
+// //         await userLogInPage.goToLogInPage()
+// //         await userLogInPage.logInUser(UN,pass)
+// //         await expect(browser).toHaveTitle("My Cart")
+// //     })
+
+// //     it(`store log in time`,()=>{
+//         var d = new Date()
+//         console.log(d.getDate());
+//         console.log(d);
+//         console.log(d.getHours());
+//         console.log(d.getMinutes());
+//         console.log(d.getHours());
+        
+
+
+// //     })
+
+// //     it("user log out", async () => {
+// //         await userCommanPage.UserLogOut()
+// //         await expect(browser).toHaveTitle(`Shopping Portal Home Page`)
+// //     })
+
+
+
+
+
+
+
+// // })
